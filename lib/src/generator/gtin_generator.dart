6d9e43a88264d567ea1b18044c9379b7e528471c
Vinicios Clarindo <vinicioslc@outlook.com>
2020-06-29 20:19:34 -0300

    Refactor improvements.

--- lib/src/generator/gtin_generator.dart
+++ lib/src/generator/gtin_generator.dart
@@ -3,13 +3,13 @@ import 'dart:math';
 import 'package:gtin_toolkit/src/math/calc_digit.dart';
 
 /// Generate any type of gtin and adds check digit `gtinLength` is the desired size with check digit
-String generateGTIN({gtinLength: 14}) {
-  final Random random = new Random();
+String generateGTIN({gtinLength = 14}) {
+  final Random random = Random();
   // if length == 8 will generate 7 digit
   final int min = pow(10, gtinLength - 2);
-  final int max = pow(10, gtinLength - 1);
+  final int max = 100000;
 
-  final list = (min + random.nextInt(100000)).toString().split('');
+  final list = (min + random.nextInt(max)).toString().split('');
 
   final List<int> generated = list.map(int.parse).toList();
   final generatedWithDigit =
